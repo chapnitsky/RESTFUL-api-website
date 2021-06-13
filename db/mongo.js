6a63d602eb14978674282f20253c46e06430f318
Doron Chapnitsky <69970498+chapnitsky@users.noreply.github.com>
2021-06-13 21:44:44 +0300

    fixxxxxxx

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const db = 'ellaro'
+const db = 'yael'
 mongoose.connect(`mongodb://127.0.0.1:27017/${db}`, {
     useNewUrlParser: true,
     useCreateIndex: true,
