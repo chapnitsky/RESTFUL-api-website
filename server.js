afa5b95bde7612654d83b535470720c1a0280aaa
Doron Chapnitsky <69970498+chapnitsky@users.noreply.github.com>
2021-06-13 18:08:14 +0300

    pigua shel yael

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 
 app.use('/list', express.static(path.join(__dirname, 'html/index.html')));
 app.use('/add_tour', express.static(path.join(__dirname, 'html/add_tour_form.html')));
+app.use('/add_guide', express.static(path.join(__dirname, 'html/add_guide_form.html')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
